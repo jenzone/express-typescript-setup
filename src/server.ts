79365570f2d73a8be0c03b931d64219d4ce9093e
Jenson Caparida <jensoncaparida@gmail.com>
2024-06-09 07:01:13 +0800

    initial setup

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import app from "./index";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
